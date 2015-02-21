2d05677a3cf8f70589373441d933b764540fc711
Juan Olivos <Juan52@ciencias.unam.mx>
2015-02-20 23:21:23 -0600

    Cargada la matriz "arreglo", la pueden llamar como arreglo[x][0,1] dependiendo que columna quieran tomar, yo creo que este debe de ser el main del programa, opinen.

---
+++ matriz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "entradatexto.h"
+int tot(),x,c;
+float h,v1,v2;
+char na;
+FILE *f;
+int main(){
+    x=tot();
+    c=0;
+    float arreglo[x][2];
+    f = fopen("datos.txt","r");
+    while((h = fgetc(f))!=EOF){
+        fscanf(f, "%f %f", &v1, &v2);
+        while(c<=x){
+            arreglo[c][0] = v1;
+            arreglo[c][1] = v2;
+            printf("%f %f\n", arreglo[c][0], arreglo[c][1]);
+            c++;
+        }
+    }
+    
+    fclose(f);
+}
