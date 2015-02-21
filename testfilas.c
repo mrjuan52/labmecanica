f02ee1028c34072f6fc4f7d0c256a9bcd4d2c3a7
Juan Olivos <Juan52@ciencias.unam.mx>
2015-02-20 22:36:37 -0600

    Listo, pruebenlo. :D

---
+++ testfilas.c
@@ -0,0 +1,9 @@
+//
+//  testfilas.c
+//  
+//
+//  Created by juan52 on 20/02/15.
+//
+//
+
+#include <stdio.h>
---
+++ testfilas.h
@@ -0,0 +1,14 @@
+//
+//  testfilas.h
+//  
+//
+//  Created by juan52 on 20/02/15.
+//
+//
+
+#ifndef ____testfilas__
+#define ____testfilas__
+
+#include <stdio.h>
+
+#endif /* defined(____testfilas__) */
