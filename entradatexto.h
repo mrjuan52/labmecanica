3d05500cc057e09da075ae3fc350d5fd2bbbab26
Juan Olivos <Juan52@ciencias.unam.mx>
2015-02-20 22:23:19 -0600

    Añadidos los archivos: datos.txt y entradatexto.h, revisenlo y chequen si hace falta algo o tienen mejor manera de realizar el conteo de lineas, este lo hice para contar el numero exacto de lineas que tiene que tener nuestra matriz. En un momento subo un archivo para que puedan ejecutar el header. Saludos.

---
+++ entradatexto.h
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+FILE *f;
+int total;
+char ch;
+int tot(void){
+    total=0;
+    f = fopen("datos.txt","r");
+    if (f == NULL){
+        printf("\nError de apertura. \n\n");
+    }else{
+        while((ch = fgetc(f))!= EOF){
+            if(ch=='\n'){
+              total++;
+            }
+        }
+    }
+    fclose(f);
+    return(total);
+}
